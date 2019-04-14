3cce559746b02398f92cfe11ae7713bc09760896
aditya <adityasingh222247@gmail.com>
2019-04-14 21:19:19 +0530

    Rest API, clients and CORS headers

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const feedRoutes = require('./routes/feed');
 //application/json
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 
