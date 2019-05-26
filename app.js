43b0b93400b5feb0e71bd27a8d9e9bb8e17532e3
Aditya Singh <adityasingh222247@gmail.com>
2019-05-26 20:44:11 +0530

    Handling errors

--- app.js
+++ app.js
@@ -44,7 +44,16 @@ app.use((req, res, next) => {
 app.use('/graphql', graphqlhttp({
     schema: graphqlSchema,
     rootValue: graphqlresolver,
-    graphiql: true
+    graphiql: true,
+    formatError(error) {
+        if (!error.originalError) {
+            return error
+        }
+        const data = error.originalError.data
+        const message = error.message || 'An error occured'
+        const code = error.originalError.code || 500
+        return { message: message, status: code, data: data }
+    }
 
 }))
 
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -14,6 +14,8 @@ module.exports = {
         }
         if (errors.length > 0) {
             const error = new Error('Invalid input')
+            error.data = errors
+            error.code = 422
             throw error
         }
         const existinguser = await User.findOne({ email: args.userInput.email })
